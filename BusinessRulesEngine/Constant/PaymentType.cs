5f935bff0d4f05d3281d188ed6ada90911ddbb62
kamendra78 <66951885+kamendra78@users.noreply.github.com>
2020-06-16 01:41:41 +0530

    Add files via upload

---
+++ BusinessRulesEngine/Constant/PaymentType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRulesEngine.Constant
+{
+        public enum PaymentType
+        {
+            PhysicalProduct = 1,
+            Book = 2,
+            MembershipActivate = 3,
+            MembershipUpgrade = 4,
+            Video = 5
+        }
+    
+}
