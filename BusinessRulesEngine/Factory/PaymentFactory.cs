e823d6896ebd2de04e66cb92b2d588b57418a65a
kamendra78 <66951885+kamendra78@users.noreply.github.com>
2020-06-16 01:44:05 +0530

    Add files via upload

---
+++ BusinessRulesEngine/Factory/PaymentFactory.cs
@@ -0,0 +1,44 @@
+﻿using BusinessRulesEngine.Payments;
+
+namespace BusinessRulesEngine.Factory
+{
+    /// <summary>
+    /// Payment object Factory
+    /// More object can be added later
+    /// </summary>
+    public  class PaymentFactory
+    {
+        // Factory Method
+        public static IPayment GetPaymentObject(int objType)
+        {
+            IPayment _IPayment;
+  
+            if (objType == Constant.PaymentType.PhysicalProduct.GetHashCode())
+            {
+                _IPayment = new PhysicalProductPayment();
+            }
+            else if (objType == Constant.PaymentType.Book.GetHashCode())
+            {
+                _IPayment = new BookPayment();
+            }
+            else if (objType == Constant.PaymentType.MembershipActivate.GetHashCode())
+            {
+                _IPayment = new MembershipActivatePayment();
+            }
+            else if (objType == Constant.PaymentType.MembershipUpgrade.GetHashCode())
+            {
+                _IPayment = new MembershipUpgradePayment();
+            }
+            else if (objType == Constant.PaymentType.Video.GetHashCode())
+            {
+                _IPayment = new VideoPayment();
+            }
+            else
+            {
+                _IPayment = null;
+            }
+            return _IPayment;
+        }
+
+    }
+}
