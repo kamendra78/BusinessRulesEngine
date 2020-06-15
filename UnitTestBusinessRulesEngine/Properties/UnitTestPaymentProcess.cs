e06d1e7468dbd3bd18ddca705f83bf39618cc329
kamendra78 <66951885+kamendra78@users.noreply.github.com>
2020-06-16 01:52:24 +0530

    Add files via upload

---
+++ UnitTestBusinessRulesEngine/Properties/UnitTestPaymentProcess.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessRulesEngine;
+using BusinessRulesEngine.Constant;
+using BusinessRulesEngine.Factory;
+using BusinessRulesEngine.Payments;
+
+namespace UnitTestBusinessRulesEngine
+{
+    [TestClass]
+    public class UnitTestPaymentProcess
+    {
+
+        [TestMethod]
+        public void PaymentFactory_Should_Generate_PackagingSlip_ForPhysicalProduct()
+        {
+            //Arrange
+            int physicalProduct = PaymentType.PhysicalProduct.GetHashCode();
+            IPayment paymentObject = PaymentFactory.GetPaymentObject(physicalProduct);
+            //Act
+            bool result = paymentObject.ProcessPayment();
+            //Assert
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void PaymentFactory_Create_DuplicatePackagingSlip_RoyaltyDep_ForBook()
+        {
+            //Arrange
+            int book = PaymentType.Book.GetHashCode();
+            IPayment paymentObject = PaymentFactory.GetPaymentObject(book);
+            //Act
+            bool result = paymentObject.ProcessPayment();
+            //Assert
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void PaymentFactory_Should_ActiavteMembership()
+        {
+            //Arrange
+            int activate = PaymentType.MembershipActivate.GetHashCode();
+            IPayment paymentObject = PaymentFactory.GetPaymentObject(activate);
+            //Act
+            bool result = paymentObject.ProcessPayment();
+            //Assert
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void PaymentFactory_Should_UpgradeMembership()
+        {
+            //Arrange
+            int upgrade = PaymentType.MembershipUpgrade.GetHashCode();
+            IPayment paymentObject = PaymentFactory.GetPaymentObject(upgrade);
+            //Act
+            bool result = paymentObject.ProcessPayment();
+            //Assert
+            Assert.AreEqual(result, true);
+        }
+
+        [TestMethod]
+        public void PaymentFactory_Should_Generate_FreeVideoSlip()
+        {
+            //Arrange
+            int video = PaymentType.Video.GetHashCode();
+            IPayment paymentObject = PaymentFactory.GetPaymentObject(video);
+            //Act
+            bool result = paymentObject.ProcessPayment();
+            //Assert
+            Assert.AreEqual(result, true);
+        }
+
+
+    }
+}
